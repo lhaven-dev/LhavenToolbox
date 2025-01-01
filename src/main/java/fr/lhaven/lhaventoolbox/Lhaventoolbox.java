668cd74c79176dce0e5b722f4972548a3f04f22c
Enzo <kdenzo88@gmail.com>
2025-01-01 20:03:10 +0100

    FUCKING WORKING
    God bless wine

--- src/main/java/fr/lhaven/lhaventoolbox/Lhaventoolbox.java
+++ src/main/java/fr/lhaven/lhaventoolbox/Lhaventoolbox.java
@@ -1,10 +1,34 @@
 package fr.lhaven.lhaventoolbox;
-
+import com.cobblemon.mod.common.api.Priority;
+import com.cobblemon.mod.common.api.events.CobblemonEvents;
+import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent;
+import net.minecraft.server.network.ServerPlayerEntity;
+import kotlin.Unit;
 import net.fabricmc.api.ModInitializer;
+
+
 public class Lhaventoolbox implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.NORMAL, this::onCaptured);
+
+
+            }
+
+    private Unit onCaptured(PokemonCapturedEvent pokemonCapturedEvent) {
+        System.out.println("test");
+        System.out.println("test" + pokemonCapturedEvent.getPokemon());
+        ServerPlayerEntity player = pokemonCapturedEvent.getPlayer();
+        System.out.println("test" + player);
+        return Unit.INSTANCE;
 
     }
 }
+
+
+
+
+
+
+
